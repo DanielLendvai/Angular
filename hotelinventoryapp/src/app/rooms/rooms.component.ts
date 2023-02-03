c542cd3272b9e480201d95bec0411adfc0de467c
Daniel Lendvai <lendvai.daniel92@gmail.com>
2023-02-03 16:23:43 +0100

    first component

--- hotelinventoryapp/src/app/rooms/rooms.component.ts
+++ hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'hinv-rooms',
   templateUrl: './rooms.component.html',
-  styleUrls: ['./rooms.component.scss']
+  styleUrls: ['./rooms.component.scss'],
 })
 export class RoomsComponent {
-
+  hotelName = 'Lorem Hotel';
+  numberOfRooms = 10;
+  hideRooms = false;
+  
+  toggle() {
+    this.hideRooms = !this.hideRooms;
+  }
 }
