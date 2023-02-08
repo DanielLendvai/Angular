d9b63aeefca3e3fd8f482fc5724dc141ce166ccd
Daniel Lendvai <lendvai.daniel92@gmail.com>
2023-02-08 14:22:20 +0100

    setting up router

--- hotelinventoryapp/src/app/app-routing.module.ts
+++ hotelinventoryapp/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContainerComponent } from './container/container.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { RoomsComponent } from './rooms/rooms.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'employee',
+    component: EmployeeComponent,
+  },
+  { path: 'rooms', 
+    component: RoomsComponent 
+  },
+  {path:'', redirectTo: '/rooms', pathMatch: 'full'} //default url
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
