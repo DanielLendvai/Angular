acb812bc8f2cb422b33de10e24643b771afdbfeb
Daniel Lendvai <lendvai.daniel92@gmail.com>
2023-02-08 16:47:47 +0100

    forms, validation, displaying errors.

--- hotelinventoryapp/src/app/rooms/rooms-add/rooms-add.component.ts
+++ hotelinventoryapp/src/app/rooms/rooms-add/rooms-add.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RoomList } from '../rooms';
+import { RoomsService } from '../services/rooms.service';
 
 @Component({
   selector: 'hinv-rooms-add',
   templateUrl: './rooms-add.component.html',
-  styleUrls: ['./rooms-add.component.scss']
+  styleUrls: ['./rooms-add.component.scss'],
 })
-export class RoomsAddComponent {
+export class RoomsAddComponent implements OnInit {
+  room: RoomList = {
+    roomType: '',
+    amenities: '',
+    rating: 0,
+    humidity: 0,
+    photos: '',
+    price: 0,
+    checkinTime: new Date(),
+    checkoutTime: new Date(),
+  };
 
+  successMessage: string = '';
+
+  constructor(private roomsService: RoomsService) {}
+
+  ngOnInit(): void {}
+
+  AddRoom() {
+    this.roomsService
+      .addRoom(this.room)
+      .subscribe((data) => (this.successMessage = 'Added successfully'));
+  }
 }
