1647182f242c0f9cdffca6804a5d3f37d3791080
Daniel Lendvai <lendvai.daniel92@gmail.com>
2023-02-23 15:51:32 +0100

    creating models

--- recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  styleUrls: ['./recipe-list.component.scss'],
 })
-export class RecipeListComponent implements OnInit { 
-  recipes = [];
-  
-  constructor() { }
-  ngOnInit(){}
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Chicken Breast', 'Best served warm', 'https://easychickenrecipes.com/wp-content/uploads/2020/05/featured-baked-chicken-breast-reshoot.jpg'),
+    new Recipe('Chicken Breast', 'Best served warm', 'https://easychickenrecipes.com/wp-content/uploads/2020/05/featured-baked-chicken-breast-reshoot.jpg'),
+  ];
+
+  constructor() {}
+  ngOnInit() {}
 }
