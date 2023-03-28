76f57cc55a2d4d46b7b7d8f2948fffd7bc930323
Daniel <lendvai.daniel92@gmail.com>
2023-03-28 10:45:39 +0200

    tailwind css

--- recipe-book/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ recipe-book/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -16,16 +16,19 @@ import { Ingredient } from 'src/app/shared/ingredient.model';
 export class ShoppingEditComponent implements OnInit {
   @ViewChild('nameInput') nameInputRef: ElementRef;
   @ViewChild('amountInput') amountInputRef: ElementRef;
-  //  ingredientAdded = new EventEmitter<{ name: string; amount: number }>();
+
   @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() {}
 
   ngOnInit() {}
+
   onAddItem() {
     const ingName = this.nameInputRef.nativeElement.value;
     const ingAmount = this.amountInputRef.nativeElement.value;
     const newIngredient = new Ingredient(ingName, ingAmount);
-    this.ingredientAdded.emit(newIngredient);
+    if(ingName && ingAmount > 0) {
+      this.ingredientAdded.emit(newIngredient);
+    }
   }
 }
